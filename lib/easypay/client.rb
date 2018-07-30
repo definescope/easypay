4887bf713edbb4d74dbc4ccbaa02c93182a00b57
Tiago Fernandes <tjg.fernandes@gmail.com>
2018-07-30 11:18:21 +0100

    allow switch easypay environment from engine config

--- lib/easypay/client.rb
+++ lib/easypay/client.rb
@@ -103,8 +103,8 @@ module Easypay
       current_args[:ep_entity] ||= @easypay_entity
       current_args[:ep_ref_type] ||= @easypay_ref_type
       current_args[:ep_country] ||= @easypay_country
-      current_args[:s_code] ||= @easypay_code if current_args[:s_code].nil? and !Rails.env.production?
-      current_args[:ep_test] = "ok" if current_args[:ep_test].nil? and !Rails.env.production?
+      current_args[:s_code] ||= @easypay_code if current_args[:s_code].nil? and !(Easypay::Engine.config.try(:mode) == 'production')
+      current_args[:ep_test] = "ok" if current_args[:ep_test].nil? and !(Easypay::Engine.config.try(:mode) == 'production')
 
       return current_args
     end
